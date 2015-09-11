88b783ce16d457dcd1373cec1a550e57c03a127c
Yu-Chun Chang <ycc123@gmail.com>
2015-09-11 16:10:13 +0800

    修正filter沒有作用的問題，原因是呼叫另外一個icalab3_lis.m這支刻意拿掉filter的程式

---
+++ test_cf_icalab3.m
@@ -0,0 +1,4 @@
+load data2.mat;
+figure;hold all;
+plot(data2(11,4).data(15).ecapICA32);
+plot(data2(11,4).data(15).filter_ecapICA32);
---
+++ test_icalab3.m
@@ -0,0 +1,5 @@
+load data2.mat;
+data=icalab3('src42v2\','erica',10,13000);
+figure;hold all;
+plot(data(15).ecapICA32);
+plot(data(15).filter_ecapICA32);
