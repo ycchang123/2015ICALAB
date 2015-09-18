bc1a6cca7c66598e0eedc5d3c39b6fa6da9b7a35
Yu-Chun Chang <ycc123@gmail.com>
2015-09-19 06:57:25 +0800

    modify README.md ,add fft_icalab3 document

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 2015ICALAB
+
+###[x,y]= fft_icalab3(input_signal)
+convert time domina to freqency domain
+
+
--- test_cf_icalab3.m
+++ test_cf_icalab3.m
@@ -6,19 +6,19 @@ plot(data2(11,4).data(15).ecap32);
 plot(data2(11,4).data(15).ecapICA32);
 plot(data2(11,4).data(15).filter_ecapICA32);
 [b,a]=butter(10,1/(56000/2),'high');
-filter_ecap32=filter(b,a,data2(11,4).data(15).ecapICA32);
-plot(filter_ecap32);
-
+filter_ecapICA32=filter(b,a,data2(11,4).data(15).ecapICA32);
+plot(filter_ecapICA32);
+hpf_filter_ecapICA32=filter(b,a,filter_ecapICA32);
+plot(hpf_filter_ecapICA32);
+legend ('AP ecap','ICALAB ecap ','LPF ICALAB ecap 13K','1 Hz HPF ICA ecap','BPF 1Hz 13K ICA ecap');
 figure; hold all;
 [x,y]=fft_icalab3(data2(11,4).data(15).ecap32);plot(x,y);
 [x,y]=fft_icalab3(data2(11,4).data(15).ecapICA32);plot(x,y);
 [x,y]=fft_icalab3(data2(11,4).data(15).filter_ecapICA32);plot(x,y)
-
-
-[x,y]=fft_icalab3(filter_ecap32);
-plot(x,y);
-
-legend ('AP ecap','ICALAB ecap ','LPF ICALAB ecap');
+[x,y]=fft_icalab3(filter_ecapICA32);plot(x,y);
+[x,y]=fft_icalab3(hpf_filter_ecapICA32);plot(x,y);
+title ('ecap spectrum')
+legend ('AP ecap','ICALAB ecap','LPF ICALAB ecap cutoff freq=13k','1 Hz HPF ecap','BPF 1Hz 13K ICA ecap');
 %figure; hold all;
 %plot(data2(6,4).data(15).ecap32);
 %plot(data2(6,4).data(15).ecapICA32);
